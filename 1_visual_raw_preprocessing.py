7ead1e1a85ac07cf45a0e2b707199d6bc5769eaa
测试账户 <uais_common@uais.lzu.edu.cn>
2016-12-05 22:54:02 +0800

    little correction of 1_visual_raw_preprocessing

--- 1_visual_raw_preprocessing.py
+++ 1_visual_raw_preprocessing.py
@@ -21,7 +21,7 @@ do_pre = False # data preprocessing
 do_fil = True # data filtering
 do_epo = True # Crop data into epochs
 res_name, tri_name = 'STI 013', 'STI 014'
-
+ev_id = 1
 ###################################
 # Raw data preprocessing
 #----------------------------------
@@ -41,7 +41,6 @@ if do_pre:
         #apply noise reducer for frequencies below 5 Hz
         noise_reducer(fn_raw_nr, reflp=5, fnout=fn_raw_nr)# plot power spectrum
         fn_power_spect = fn_raw_nr[:fn_raw_nr.rfind('-raw.fif')] + ',denoising'
-        ev_id = 1
         plot_denoising([fn_raw, fn_raw_nr], stim_name=tri_name,
                     event_id=ev_id, show=False, fnout=fn_power_spect,
                     tmin_stim=-0.4, tmax_stim=0.4)
@@ -63,11 +62,11 @@ if do_pre:
 #----------------------------------
 
 if do_fil:
-    fn_list = glob.glob(subjects_dir + '/*[0-9]/MEG/*ocarta-raw.fif')
+    fn_list = glob.glob(subjects_dir + '/*[0-9]/MEG/*,nr,ocarta-raw.fif')
     for fn_raw_cl in fn_list:
         fn_raw_fil = fn_raw_cl[:fn_raw_cl.rfind('-raw.fif')] + ',fibp1-45-raw.fif'
         apply_filter(fn_raw_cl, flow=1, fhigh=45, order=4, njobs=4)
-        jumeg_plot.plot_compare_brain_responses(fn_raw, fn_raw_fil, event_id=ev_id, stim_name='trigger')
+        jumeg_plot.plot_compare_brain_responses(fn_raw_cl, fn_raw_fil, event_id=ev_id, stim_name='trigger')
         
 ###################################
 # Crop the data
